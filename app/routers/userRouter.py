d322468a82b40809103b647f5c4f048c3c85dd72
wissembhk <58009610+wissembhk@users.noreply.github.com>
2022-07-23 07:57:21 +0100

    Update userRouter.py

--- app/routers/userRouter.py
+++ app/routers/userRouter.py
@@ -4,6 +4,6 @@ from models.schemas import UserSchema
 router = APIRouter(prefix="/user")
 
 
-@router.post('/user')
-async def create_userr(user: UserSchema):
+@router.post('/add')
+async def create_user_router(user: UserSchema):
     return await create_user(user)
